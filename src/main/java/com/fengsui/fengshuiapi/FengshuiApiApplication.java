004da00bf636de95e9d45b3791f73ac73fcb0b53
nhatanhfaber <nguyenducnhatanh@fabercompany.co.jp>
2020-10-18 23:00:23 +0700

    init

---
+++ src/main/java/com/fengsui/fengshuiapi/FengshuiApiApplication.java
@@ -0,0 +1,13 @@
+package com.fengsui.fengshuiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FengshuiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FengshuiApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fengsui/fengshuiapi/FengshuiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengsui.fengshuiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FengshuiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
